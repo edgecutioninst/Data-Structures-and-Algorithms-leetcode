ff2e7c304f94235e82c314f57b310b83832fea94
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-02-09 21:35:21 +0530

    Time: 2 ms (76.88%), Space: 18.6 MB (43.74%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,47 +1,61 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {   
-        
-        vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
 
+    bool dfs(int node, vector<vector<int>>& adj, vector<int>& visited, vector<int>& pathVisited, stack<int>& st) 
+    {
+        visited[node] = 1;
+        pathVisited[node] = 1; // marked currently in recursion stack
+        
+        for(int i = 0; i < adj[node].size(); i++) 
+        {
+            int neighbor = adj[node][i];
+            
+            if(!visited[neighbor]) 
+            {
+                if(dfs(neighbor, adj, visited, pathVisited, st)) return true; // cycle found deeper, return back 1
+            }
+
+            //cycle found
+            else if(pathVisited[neighbor]) 
+            return true;
+        }
+        
+        pathVisited[node] = 0;
+        
+        //push on the way back and backtrack the path aswelll
+        st.push(node);
+        
+        return false; 
+    }
+
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        vector<vector<int>> adj(numCourses);
         for(int i = 0; i < prerequisites.size(); i++) 
         {
-            int u = prerequisites[i][0];  // want to take
-            int v = prerequisites[i][1]; //must take first
-
-            adj[v].push_back(u); //prereq -> course
-            indegree[u]++;
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u); // prereq -> course
         }
-
-        queue<int> q;
-        for(int i = 0; i < numCourses; i++) 
-        if(indegree[i] == 0) q.push(i);
         
-
-        vector<int> ans; 
-
-        while(!q.empty()) 
-        {
-            int node = q.front();
-            q.pop();
-            
-            //ans goes here
-            ans.push_back(node); 
-
-            for(int i = 0; i < adj[node].size(); i++) 
-            {
-                int neighbor = adj[node][i];
-                indegree[neighbor]--;
-                
-                if(indegree[neighbor] == 0) q.push(neighbor);
+        vector<int> visited(numCourses, 0);
+        vector<int> pathVisited(numCourses, 0);
+        stack<int> st;
+        
+        for(int i = 0; i < numCourses; i++) {
+            if(!visited[i]) {
+                //if (true) -> cycle -> cant be sorted
+                if(dfs(i, adj, visited, pathVisited, st)) return {}; 
             }
         }
-
-        //if no cycle
-        if(ans.size() == numCourses) return ans;
         
-        //cycle found
-        return {};
+        //reverses the order giving the perfect sort
+        vector<int> ans;
+        while(!st.empty()) {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        return ans;
     }
 };
