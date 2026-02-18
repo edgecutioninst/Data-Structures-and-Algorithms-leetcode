789ed010a33f4da32eb2a7406c76451e74376a66
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-02-18 20:21:39 +0530

    Time: 0 ms (100%), Space: 10.1 MB (78.25%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -15,18 +15,18 @@ public:
 
 
     // B. TABULATE: 
-    int solve2(vector<int>& nums, int n, vector<int> &dp)
-    {
-        if(n == 0) return nums[0];
-        if(n < 0) return 0;
+    // int solve2(vector<int>& nums, int n, vector<int> &dp)
+    // {
+    //     if(n == 0) return nums[0];
+    //     if(n < 0) return 0;
 
-        if(dp[n] != -1) return dp[n];
+    //     if(dp[n] != -1) return dp[n];
 
-        int incl = solve2(nums, n-2, dp) + nums[n];
-        int excl = solve2(nums, n-1, dp);
+    //     int incl = solve2(nums, n-2, dp) + nums[n];
+    //     int excl = solve2(nums, n-1, dp);
 
-        return dp[n] = max(incl,excl);
-    }
+    //     return dp[n] = max(incl,excl);
+    // }
 
 
     int rob(vector<int>& nums) {
@@ -34,8 +34,36 @@ public:
         // return solve(nums, nums.size() - 1);
 
         // B: Tabulate
-        vector<int>dp(nums.size()+1, -1);
-        return solve2(nums, nums.size() - 1,dp);
+        // vector<int>dp(nums.size()+1, -1);
+        // return solve2(nums, nums.size() - 1,dp);
+
+        // C: Bottom Up
+        
+        int n = nums.size();
+
+        if (n == 0) return 0;
+        if (n == 1) return nums[0];
+
+        // vector<int>dp(n + 1, INT_MIN);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+        // use 3 variables instead since it depends on -2 and -1 only
+
+        int prev2 = 0;  
+        int prev1 = nums[0];  
+
+        for(int i = 1; i < n; i++)
+        {
+            int incl = prev2 + nums[i]; 
+            int excl = prev1;
+            int ans = max(incl,excl);
+
+            prev2 = prev1;
+            prev1 = ans;
+        }
+
+        return prev1;
+
 
 
     }
