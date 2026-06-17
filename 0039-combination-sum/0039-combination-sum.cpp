bc93e63d6c1c21c0334323c68e8ccefdf42f18c0
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-06-17 14:02:28 +0530

    Time: 3 ms (52.03%), Space: 14.1 MB (74.57%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
 
-    void combination (vector<int> &candidates, int x, vector<vector<int>> &ans, vector<int> &temp, int sum, int i)
-    {
-        if(i == candidates.size())
+        void combination (vector<int> &candidates, int x, vector<vector<int>> &ans, vector<int> &temp, int sum, int i)
         {
-            if(sum == x)
-            {
-                ans.push_back(temp);
-            }
-            return;
-        }
-
-        if (sum + candidates[i] <= x) 
-        {
-            temp.push_back(candidates[i]);
-    
-            combination(candidates, x, ans, temp, sum + candidates[i], i); 
             
-            temp.pop_back();                 
+            if(i == candidates.size())
+            {
+                if(sum == x)
+                {
+                    ans.push_back(temp);
+                }
+                return;
+            }
+
+            if (sum + candidates[i] <= x) 
+            {
+                temp.push_back(candidates[i]);
+                combination(candidates, x, ans, temp, sum + candidates[i], i); 
+                temp.pop_back();                 
+            }
+
+            combination(candidates, x, ans, temp, sum, i + 1);
         }
 
-        //moves to next:
-        combination(candidates, x, ans, temp, sum, i+1);
-    }
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) 
-    {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 
         vector<vector<int>> ans;
         vector<int> temp;
