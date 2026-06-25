32089a30c46430e097196680e7357a3469627ced
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-06-25 16:48:01 +0530

    Time: 21 ms (92.14%), Space: 17.6 MB (43.16%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
 
-
     void dfs(int row, int col, vector<vector<char>> &grid , vector<vector<int>> &vis) 
     {
-        
-
         int n = grid.size();
         int m = grid[0].size();
 
@@ -13,28 +10,25 @@ public:
             row < 0 || col < 0 || row >= n || col >= m 
             || grid[row][col] == '0' || vis[row][col] == 1
         )   return;
-    
-        vis[row][col] = 1;
 
+        vis[row][col] = 1;
 
         dfs(row - 1, col, grid, vis); 
         dfs(row + 1, col, grid, vis); 
         dfs(row, col - 1, grid, vis); 
         dfs(row, col + 1, grid, vis); 
-
     }
 
-    int numIslands(vector<vector<char>>& grid) 
-    {
+    int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         
         vector<vector<int>> visited(n, vector<int>(m, 0)); 
         int count = 0;
 
-        for(int row = 0; row < grid.size(); row++)
+        for(int row = 0; row < n; row++)
         {
-            for(int col = 0; col<grid[0].size(); col++)
+            for(int col = 0; col < m; col++)
             {
                 if(!visited[row][col] && grid[row][col] == '1') 
                 {
