5a424e5f8bddbcd3473154b8a1a752a31d551e37
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-06-28 23:10:29 +0530

    Time: 2 ms (79.01%), Space: 19.4 MB (54.85%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
-    {   
-        //adj list:
+    bool dfs(int node, vector<vector<int>> &adj,vector<bool> &visited, vector<bool> &inPath)
+    {
+        inPath[node] = true;
+
+        for(int j = 0; j < adj[node].size(); j++) 
+        {
+            int neigh = adj[node][j];
+
+            if(inPath[neigh]) return true; // cycle
+            
+            if(!visited[neigh])
+                if(dfs(neigh, adj, visited, inPath)) return true;
+        }
+
+        inPath[node] = false;
+        visited[node] = true;
+        return false; // no cycle found
+        }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses, 0);
 
         for(int i = 0; i < prerequisites.size(); i++)
         {
-            int u = prerequisites[i][0];  // want to take
-            int v = prerequisites[i][1]; //must take first
+            int u = prerequisites[i][0];  // course
+            int v = prerequisites[i][1]; //pre req
 
             adj[v].push_back(u); //prereq -> course
-            indegree[u]++;
         }
 
-        queue<int> q;
+        vector<bool> visited(numCourses, false);
+        vector<bool> inPath(numCourses, false);
 
-        for(int i = 0; i < numCourses; i++) 
-        {
-            if(indegree[i] == 0) 
-            q.push(i);
-        }
+        for(int i = 0; i < numCourses; i++)
+            if(!visited[i])
+                if(dfs(i, adj, visited, inPath)) return false;
 
-        int coursesFinished = 0;
+        return true;
 
-        while(!q.empty())
-        {
-            int node = q.front();
-            q.pop();
-            coursesFinished++; // finished current course.
-
-           for(int i = 0; i < adj[node].size(); i++) 
-           {
-                int neighbor = adj[node][i];
-
-                indegree[neighbor]--;
-                
-                if(indegree[neighbor] == 0) 
-                q.push(neighbor);
-                
-            }
-        }
-
-        return coursesFinished == numCourses;
-
-    }
+    }       
 };
