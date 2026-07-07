9a04a7879733cdb52bd19c055ed5ed96d05db35a
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-07-07 23:08:06 +0530

    Time: 914 ms (11.59%), Space: 180.3 MB (19.96%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,43 +1,71 @@
 class Solution {
 public:
-    int minCostConnectPoints(vector<vector<int>>& points) {
 
-        int n = points.size();
-        vector<bool> vis(n, false);
-
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>
-        > pq;
-
-        int ans = 0;
-                //wt, node(0 weight of node at 0th index of points array)
-        pq.push({0, 0});
-
-        while(!pq.empty())
+    int find(vector<int>& parent, int x) 
+    {
+        while(parent[x] != x) 
         {
-            int wt = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
+            x = parent[x];
+        }
+        return x;
+    }
 
-            if(vis[node]) continue;
 
-            vis[node] = 1;
+    bool unite(vector<int>& parent, vector<int>& rank, int x, int y) 
+    {
+        int px = find(parent, x), py = find(parent, y);
+        if(px == py) return false; // cycle
 
-            ans += wt;
+        if(rank[px] > rank[py]) 
+        parent[py] = px;
 
-            for(int v = 0; v < n; v++)
+        else if(rank[px] < rank[py]) 
+        parent[px] = py;
+
+        else 
+        { 
+            parent[py] = px; 
+            rank[px]++; 
+        }
+
+        return true;    
+    }
+
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        int n = points.size();
+
+        vector<vector<int>> edges;
+
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < n; j++)
             {
-                if(vis[v])  continue;
-                
-                int wt = abs(points[node][0] - points[v][0]) +
-                         abs(points[node][1] - points[v][1]);
-
-                pq.push({wt, v});        
+                int wt = abs(points[i][0] - points[j][0]) +
+                         abs(points[i][1] - points[j][1]);
+                edges.push_back({wt, i, j});
             }
         }
 
+        sort(edges.begin(), edges.end());
+
+        vector<int> parent(n);
+        vector<int> rank(n, 0);
+
+        for(int i = 0; i < n; i++)
+        parent[i] = i;
+
+        int ans = 0;
+
+        for(int i = 0; i < edges.size(); i++)
+        {
+            int wt = edges[i][0];
+            int u  = edges[i][1];
+            int v  = edges[i][2];
+
+            if(unite(parent, rank, u, v))
+            ans += wt;
+        }
         return ans;
     }
 };
