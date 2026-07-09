dc1bd360011dc76d2005a4f40a7b8bd9a40321ba
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-07-10 00:22:31 +0530

    Time: 34 ms (54.43%), Space: 19.1 MB (57.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,60 +1,32 @@
 class Solution {
 public:
 
-    //A: 
-    // int recurse(vector<int>& coins, int amount, vector<int>&dp)
-    // {
-    //     if(amount == 0) return 0;
+    int recurse(vector<int>& coins, int amount, vector<int>&dp)
+    {
+        if(amount == 0) return 0;
 
-    //     if(amount < 0) return INT_MAX;
+        if(amount < 0) return INT_MAX;
 
-    //     if(dp[amount] != -1) return dp[amount];
+        if(dp[amount] != -1) return dp[amount];
 
-    //     int mini = INT_MAX;
+        int ans = INT_MAX;
 
-    //     for(int i = 0; i<coins.size(); i++)
-    //     {
-    //         int ans = recurse(coins, amount - coins[i], dp);
+        for(int i = 0; i<coins.size(); i++)
+        {
+            int _ = recurse(coins, amount - coins[i], dp);
 
-    //         if (ans != INT_MAX) 
-    //         mini = min(1 + ans, mini);
-            
-    //     }
-        
-    //     return dp[amount] = mini;
-    // }
-
-
+            if (_ != INT_MAX) 
+            ans = min(1 + _, ans);
+        }
+        return dp[amount] = ans;
+    }
 
 
     int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, -1); 
 
-        // vector<int> dp(amount + 1, -1); 
+        int ans = recurse(coins, amount, dp);
 
-        // int ans = recurse(coins, amount, dp);
-
-        // return (ans!=INT_MAX) ? ans : -1;
-        // ^  A ends here ^
-
-        // B begins here:
-
-        vector<int> dp(amount+1, INT_MAX);
-
-        dp[0] = 0;
-
-        for(int i = 1; i <= amount; i++)
-        {
-            for (int j = 0; j < coins.size(); j++) {
-                
-                if (i - coins[j] >= 0) 
-                {
-                    
-                    if (dp[i - coins[j]] != INT_MAX) 
-                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
-                }
-            }
-        }
-
-        return (dp[amount] != INT_MAX) ? dp[amount] : -1;
+        return (ans!=INT_MAX) ? ans : -1;
     }
 };
