9b0b84cc86400f8c721e56eb8cc4a1ad941d245c
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-07-12 23:45:56 +0530

    Time: 50 ms (7.41%), Space: 27.6 MB (30.92%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,46 +1,25 @@
 class Solution {
 public:
 
-    // int memoise(string &x, string &y, int n, int m, vector<vector<int>> &dp)
-    // {
-    //     if(n == 0 || m == 0) return 0;
+    int solve(int i, int j, string &text1, string &text2, vector<vector<int>> &dp)
+    {
+        if(i == text1.size() || j == text2.size()) return 0;
 
-    //     if(dp[n][m] != -1) return dp[n][m];
+        if(dp[i][j] != -1) return dp[i][j];
 
-    //     if(x[n-1] == y[m-1]) 
-    //     return dp[n][m] = 1 + memoise(x,y,n-1,m-1,dp);
+        if(text1[i] == text2[j]) 
+        return dp[i][j] = 1 + solve(i + 1, j + 1, text1, text2, dp);
 
-    //     else 
-    //     return dp[n][m] = max(
-    //         memoise(x,y,n-1,m,dp),
-    //         memoise(x,y,n,m-1,dp)
-    //     );
-    // }
+        return dp[i][j] = max( solve(i + 1, j, text1, text2, dp), solve(i, j + 1, text1, text2, dp) );   
+    }
 
     int longestCommonSubsequence(string text1, string text2) {
-       
-        int n = text1.size();
-        int m = text2.size();
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<vector<int>> dp(
+            text1.size(),
+            vector<int>(text2.size(), -1)
+        );
 
-        // return memoise(text1, text2, n, m, dp);
-
-        for(int i = 1; i <= n; i++) 
-        {
-            for(int j = 1; j <= m; j++) 
-            {
-                
-                if(text1[i-1] == text2[j-1]) 
-                dp[i][j] = 1 + dp[i-1][j-1];
-                
-                else 
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-                
-            }
-        }
-
-        return dp[n][m];
-
+        return solve(0, 0, text1, text2, dp);
     }
 };
