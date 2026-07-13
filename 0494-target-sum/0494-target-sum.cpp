c14e881cf16e0a1cb8d3adf40dc4022dc8d0f24d
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-07-13 21:17:50 +0530

    Time: 406 ms (22.79%), Space: 90 MB (5.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,42 +1,23 @@
 class Solution {
 public:
 
-    int countSubsets(vector<int>& arr, int target) 
+    int solve(int i, int sum, vector<int>&nums, int &target, unordered_map<string, int> &dp)
     {
-        int n = arr.size();
+        if(i == nums.size())
+            return (sum == target);
 
-        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        string key = to_string(i) + "," + to_string(sum);
 
-        dp[0][0] = 1; 
+        if(dp.find(key) != dp.end()) return dp[key];
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= target; j++) 
-            { 
-                if (arr[i - 1] <= j) 
-                dp[i][j] = (dp[i - 1][j - arr[i - 1]] + dp[i - 1][j]);
-                
-                else 
-                dp[i][j] = dp[i - 1][j];
-            
-            }
-        }
-
-        return dp[n][target];
+        return dp[key] = 
+            solve(i + 1, sum + nums[i], nums, target, dp) +
+            solve(i + 1, sum - nums[i], nums, target, dp);
     }
 
+    int findTargetSumWays(vector<int>& nums, int target) {
+        unordered_map<string, int> dp;
 
-    int findTargetSumWays(vector<int>& nums, int target) 
-    {
-
-        int totalSum = 0;
-        for (int x : nums) totalSum += x;
-
-        if (totalSum < abs(target) || (totalSum + target) % 2 != 0 || (totalSum + target) < 0)
-        return 0;
-        
-
-        int s1 = (totalSum + target) / 2;
-        return countSubsets(nums, s1);
-        
+        return solve(0, 0, nums, target, dp);   
     }
 };
