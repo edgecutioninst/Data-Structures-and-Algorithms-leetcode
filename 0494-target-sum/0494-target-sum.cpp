bcc6227c13400b2428c2c4e36f7e8cff196e23d4
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-08-08 18:40:25 +0530

    Time: 0 ms (100%), Space: 15.3 MB (57.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
 
-    int solve(int i, int sum, vector<int>&nums, int &target, unordered_map<string, int> &dp)
+    int solve(int i, vector<int> &nums, int target, vector<vector<int>> &dp)
     {
         if(i == nums.size())
-            return (sum == target);
+        {
+            if(target == 0) return 1;
+            else return 0;
+        }
+    
+        if(dp[i][target] != -1) return dp[i][target];
 
-        string key = to_string(i) + "," + to_string(sum);
+        int take = 0;
+        if (nums[i] <= target)
+        take = solve(i + 1, nums, target - nums[i], dp);
 
-        if(dp.find(key) != dp.end()) return dp[key];
+        int skip = solve(i + 1, nums, target, dp);
 
-        return dp[key] = 
-            solve(i + 1, sum + nums[i], nums, target, dp) +
-            solve(i + 1, sum - nums[i], nums, target, dp);
+        return dp[i][target] = take + skip;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        unordered_map<string, int> dp;
+        int total = 0;
 
-        return solve(0, 0, nums, target, dp);   
+        for (int x : nums) total += x;
+
+        if (abs(target) > total) return 0;
+
+        if ((total + target) % 2 != 0) return 0;
+
+        int required = (total + target) / 2;
+
+        vector<vector<int>> dp(
+            nums.size(),
+            vector<int>(required + 1, -1)
+        );
+
+        return solve(0, nums, required, dp);
     }
 };
