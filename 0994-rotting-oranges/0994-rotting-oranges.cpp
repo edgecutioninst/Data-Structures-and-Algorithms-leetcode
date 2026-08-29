320d89824248590fe850b39d581fe686c97b03eb
Harsh Kumar <142533971+edgecutioninst@users.noreply.github.com>
2026-08-29 19:13:40 +0530

    Time: 4 ms (13.46%), Space: 16.6 MB (86.19%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int time = 0; 
-        int freshCount = 0;
+        int time = 0, freshCount = 0;
 
         queue<pair<int,int>>q;
 
@@ -10,52 +9,54 @@ public:
         {
             for(int j = 0; j < grid[0].size(); j++)
             {
-                if(grid[i][j] == 1)
-                    freshCount ++;
-                else if(grid[i][j] == 2)
-                    q.push({i,j});
+                if(grid[i][j] == 1) freshCount ++;
+                else if(grid[i][j] == 2) q.push({i,j});
             }
         }
 
-        if(!freshCount) return 0;
+        if(freshCount == 0) return 0;
 
         while(!q.empty() && freshCount > 0)
         {
             int size = q.size();
-
             for(int i = 0; i < size; i++)
             {
                 int r = q.front().first;
                 int c = q.front().second;
                 q.pop();
 
-                if(r - 1 >= 0 && grid[r-1][c] == 1){
+                if(r - 1 >= 0 && grid[r-1][c] == 1)
+                {
                     freshCount--;
                     grid[r-1][c] = 2;
                     q.push({r-1, c});
                 }
-                
-                if(r + 1 < grid.size() && grid[r+1][c] == 1) {
+
+                if(r + 1 < grid.size() && grid[r+1][c] == 1) 
+                {
                     freshCount--;
                     grid[r+1][c] = 2;
                     q.push({r+1, c});
                 }
                 
-                if(c - 1 >= 0 && grid[r][c-1] == 1) {
+                if(c - 1 >= 0 && grid[r][c-1] == 1) 
+                {
                     freshCount--;
                     grid[r][c-1] = 2;
                     q.push({r, c-1});
                 }
                 
-                if(c + 1 < grid[0].size() && grid[r][c+1] == 1) {
+                if(c + 1 < grid[0].size() && grid[r][c+1] == 1)
+                {
                     freshCount--;
                     grid[r][c+1] = 2;
                     q.push({r, c+1});
-                }   
+                }
             }
-            time += 1;
+            time ++;
         }
 
-        return (!freshCount) ? time : -1;
+        if(freshCount > 0) return -1;
+        else return time;
     }
 };
